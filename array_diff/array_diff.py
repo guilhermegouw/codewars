a6abbac18bc05b07e51f427ab64dc3295050c84c
guilhermegouw <guilherme.gouw@gmail.com>
2021-05-01 21:28:54 -0300

    array_diff_completed

--- array_diff/array_diff.py
+++ array_diff/array_diff.py
@@ -10,5 +10,8 @@ array_diff([1,2,2,2,3],[2]) == [1,3]
 """
 
 
-def array_diff(array_1, array_2):
-    pass
+def array_diff(a, b):
+    for num in b:
+        while num in a:
+            a.remove(num)
+    return a
